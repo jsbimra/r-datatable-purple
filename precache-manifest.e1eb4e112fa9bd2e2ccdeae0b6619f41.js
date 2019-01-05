ddfcb36015e0a250f59cc7d487c4612d6b6094b6
jsbimra <jsbimra@users.noreply.github.com>
2019-01-05 13:45:36 +0530

    Updates

---
+++ precache-manifest.e1eb4e112fa9bd2e2ccdeae0b6619f41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3077382d3510cfb1e502",
+    "url": "/react-dtlab/static/js/runtime~main.3077382d.js"
+  },
+  {
+    "revision": "8b605edc640afa736edc",
+    "url": "/react-dtlab/static/js/main.8b605edc.chunk.js"
+  },
+  {
+    "revision": "f23e0358980e2197562a",
+    "url": "/react-dtlab/static/js/1.f23e0358.chunk.js"
+  },
+  {
+    "revision": "8b605edc640afa736edc",
+    "url": "/react-dtlab/static/css/main.8c08ea9d.chunk.css"
+  },
+  {
+    "revision": "ad7a092ad270fb4dd1e9d5521f4ac566",
+    "url": "/react-dtlab/index.html"
+  }
+];
